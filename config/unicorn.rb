c8a640de93eb363e0571f25ea394944ef5c39e60
akshch <akshaych2786@gmail.com>
2020-02-23 11:37:11 +0530

    unicorn configuration updated

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 # set path to application
-app_dir = File.expand_path("home/deployer/apps/portal", __FILE__)
+app_dir = File.expand_path("home/deployer/apps/portal/current", __FILE__)
 shared_dir = "#{app_dir}/shared"
 working_directory app_dir
 
@@ -9,11 +9,32 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "#{shared_dir}/tmp/unicorn.portal.sock" #, :backlog => 64
 
 # Logging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
 stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
+pid "#{shared_dir}/tmp/pids/unicorn.pid"
+
+if GC.respond_to?(:copy_on_write_friendly=)
+  GC.copy_on_write_friendly = true
+end
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
